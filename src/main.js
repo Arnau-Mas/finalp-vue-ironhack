fd9a262f726158087c35dcd26bdea4dbfe2b2909
Arnau-Mas <arnau.mava@gmail.com>
2022-03-25 11:55:13 +0100

    setup tailwind

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
+import './assets/tailwind.css'
 const pinia = createPinia();
 
 createApp(App).use(router).use(pinia).mount("#app");
