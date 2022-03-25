6cbaaf9fcec5f5b7b998c636a1629c2624ce7f39
Arnau-Mas <arnau.mava@gmail.com>
2022-03-25 11:53:12 +0100

    commit

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -5,6 +5,7 @@
 <script setup>
   import SignIn from '../components/SignIn.vue';
   import SignUp from '../components/SignUp.vue';
+  //hacer un evento para el enter que haga "submit" @keyup tecla enter etc (buscar)
   import { ref } from 'vue';
 
   let route = ref("/auth/sign-up");
