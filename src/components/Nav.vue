7c071ed7d4a79e2feaf48b603c0a521e4357ea65
Arnau-Mas <arnau.mava@gmail.com>
2022-03-27 13:05:36 +0200

    navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,65 @@
 <template>
-
+ <Disclosure as="nav" class="bg-white shadow-md" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-cyan-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex-shrink-0 flex items-center">
+            <img class="block lg:hidden h-8 w-auto" src="../assets/todo-letters-logo.png" alt="Todo Nada" />
+            <img class="hidden lg:block h-8 w-auto" src="../assets/todo-letters-logo.png" alt="Workflow" />
+          </div>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex space-x-4">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? ' text-white border bg-cyan-600 shadow-sm' : 'text-gray-500 hover:bg-cyan-100', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+            </div>
+          </div>
+        </div>
+        <div class=" text-gray-500 absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <!-- Profile dropdown -->
+          <button @click="signOutApp">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    <DisclosurePanel class="sm:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-cyan-600 text-white' : 'text-gray-500 hover:bg-cyan-100 hover:text-gray-500', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <script setup>
+    import {ref} from 'vue'
+    import {useUserStore} from '../store/user.js';
+    import { useRouter } from 'vue-router';
+    import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+    import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+    const router = useRouter();
+    const user = useUserStore();
+    const navigation = [
+    { name: 'Tasks', href: '#', current: true },
+    { name: 'Archived', href: '#', current: false },
+    { name: 'Deleted', href: '#', current: false },
+    ]
+    async function signOutApp(){
+    try{
+        await user.signOut();
+        router.push({ path: '/auth' });
+    }catch(error){
+        console.log(error)
+    }
+}
 </script>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,14 @@
 <template>
   <section>
-      <h3>{{user.user.email}}</h3>
-      <NewTask/>
-      <button @click="signOutApp">Sign Out</button>
+      <Nav/>
+      <!-- <h3>{{user.user.email}}</h3> -->
+      <NewTask />
   </section>
 </template>
 
 <script setup>
     import {useUserStore} from '../store/user.js';
     import { useRouter } from 'vue-router';
+    import Nav from '../components/Nav.vue';
     import NewTask from '../components/NewTask.vue';
-    const router = useRouter();
-    const user = useUserStore();
-    async function signOutApp(){
-        try{
-            await user.signOut();
-            router.push({ path: '/auth' });
-        }catch(error){
-            console.log(error)
-        }
-    }
 </script>
