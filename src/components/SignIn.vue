ffe589232d856cd3b18f52d162e22155c1df48a4
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-04-07 18:43:14 +0200

    fixing margin and info message

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <div
-            class="min-h-full flex items-center justify-center mobile:pt-12 sm:py-12 px-4 sm:px-6 lg:px-8"
+            class="min-h-full flex items-center justify-center mobile:pt-12 sm:py-12 px-2 sm:px-6 lg:px-8"
         >
             <div class="bg-white rounded-lg p-10 max-w-md w-full space-y-8">
                 <!-- shadow-md a dalt -->
@@ -86,14 +86,14 @@
                             Sign in
                         </button>
                     </div>
-                    <div v-if="infoMessage" id="alert-1" class="flex p-4 mb-4 bg-cyan-100 rounded-lg dark:bg-cyan-200" role="alert">
-                       <svg xmlns="http://www.w3.org/2000/svg" class="text-cyan-700 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <div v-if="infoMessage" id="alert-1" class="flex p-4 mb-4 bg-red-100 rounded-lg dark:bg-red-200" role="alert">
+                       <svg xmlns="http://www.w3.org/2000/svg" class="text-red-700 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <div v-if="infoMessage" class="ml-3 text-sm font-medium text-cyan-700 dark:text-cyan-800">
-                            This is a test email in case you don't want to register.
+                        <div v-if="infoMessage" class="ml-3 text-sm text-red-700 dark:text-red-800">
+                            <span class="font-medium">trialtodoapp@gmail.com</span> is a test email in case don't want to register.
                         </div>
-                        <button @click="closeInfoMessage" type="button" class="ml-auto -mx-1.5 -my-1.5 bg-cyan-100 text-cyan-500 rounded-lg focus:ring-2 focus:ring-cyan-400 p-1.5 hover:bg-cyan-200 inline-flex h-8 w-8 dark:bg-cyan-200 dark:text-cyan-600 dark:hover:bg-blue-300" data-dismiss-target="#alert-1" aria-label="Close">
+                        <button @click="closeInfoMessage" type="button" class="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-red-200 dark:text-red-600 dark:hover:bg-red-300" data-dismiss-target="#alert-1" aria-label="Close">
                             <span class="sr-only">Close</span>
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
